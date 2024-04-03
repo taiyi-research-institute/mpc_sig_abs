ccd77dc843e76b56ba02a95c9e7a44897e6568d0
winston-wen <winston-wen@hotmail.com>
2024-04-03 14:30:59 +0800

    remove keystryoshka due to the fucking generics disaster

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 mod aes_gcm;
 mod keystore;
-mod keystryoshka;
 mod messenger;
 mod mpc_addr;
 mod pickle;
 
 pub use aes_gcm::*;
 pub use keystore::*;
-pub use keystryoshka::*;
 pub use messenger::*;
 pub use mpc_addr::*;
 pub use pickle::*;
