63220716088caadc6400725baa3a98e0d601522d
winston-wen <winston-wen@hotmail.com>
2024-04-08 23:12:41 +0800

    add default register/execute/clear

--- src/messenger.rs
+++ src/messenger.rs
@@ -1,4 +1,3 @@
-use std::collections::{BTreeMap, BTreeSet}; // To ensure that keys are in deterministic order.
 use std::fmt::Display;
 
 use super::MpcAddr;
@@ -67,4 +66,31 @@ pub trait BatchMessenger {
     where
         T: Serialize + DeserializeOwned + Send + Sync;
     async fn clear_receive(&mut self);
+
+    async fn register<T>(
+        &mut self,
+        topic: &str,
+        src: MpcAddr,
+        dst: MpcAddr,
+        seq: usize,
+        obj: &T,
+    ) -> Result<(), Self::ErrorType>
+    where
+        T: Serialize + DeserializeOwned + Send + Sync,
+    {
+        self.register_send(topic, src, dst, seq, obj).await?;
+        self.register_receive(topic, dst, src, seq).await?;
+        Ok(())
+    }
+
+    async fn execute(&mut self) -> Result<(), Self::ErrorType> {
+        self.execute_send().await?;
+        self.execute_receive().await?;
+        Ok(())
+    }
+
+    async fn clear(&mut self) {
+        self.clear_send().await;
+        self.clear_receive().await;
+    }
 }
